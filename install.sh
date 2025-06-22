cd9c6a7548f636d8245bd77a087b0568d9f9cbb0
musdx <blue.neutron460@slmail.me>
2025-06-22 10:38:32 -0400

    deb test 2+ change install script

--- install.sh
+++ install.sh
@@ -5,18 +5,14 @@ YELLOW='\033[1;32m'
 
 echo -e ${YELLOW}make directory
 
-sudo mkdir /opt/azul_box/
-
 cargo build --release
 
 echo -e ${YELLOW}copy content
 
 sudo cp -r assets/logo.png /usr/share/icons/azul_box.png
 
-sudo cp -r assets/ /opt/azul_box/
+sudo cp target/release/azul-box /usr/bin/azulbox
 
-sudo cp target/release/azul-box /opt/azul_box/
-
-sudo cp desktop/azul_box.desktop ~/.local/share/applications/
+sudo cp desktop/azul_box.desktop /usr/share/applications/
 
 echo -e ${RED}Remember to install dependencies! Check https://github.com/musdx/azul-box/blob/master/README.md#dependencies for more info
