994512fc650e98dfd2969a7d59b3b0662defd837
musdx <blue.neutron460@slmail.me>
2025-07-01 13:15:41 -0400

    added more languages

--- src/ui/shares/lang.rs
+++ src/ui/shares/lang.rs
@@ -26,7 +26,22 @@ impl LangThing {
             "ro", // Romanian
             "tr", // Turkish
             "vi", // Vietnamese
-            "ko", //Korean
+            "ko", // Korean
+            "el", // Greek
+            "he", // Hebrew
+            "th", // Thai
+            "id", // Indonesian
+            "ms", // Malay
+            "hi", // Hindi
+            "uk", // Ukrainian
+            "bg", // Bulgarian
+            "hr", // Croatian
+            "sk", // Slovak
+            "sl", // Slovenian
+            "sr", // Serbian
+            "lt", // Lithuanian
+            "lv", // Latvian
+            "et", // Estonian
         ];
         let languages: Vec<&str> = vec![
             "English",
@@ -51,26 +66,45 @@ impl LangThing {
             "Turkish",
             "Vietnamese",
             "Korean",
+            "Greek",
+            "Hebrew",
+            "Thai",
+            "Indonesian",
+            "Malay",
+            "Hindi",
+            "Ukrainian",
+            "Bulgarian",
+            "Croatian",
+            "Slovak",
+            "Slovenian",
+            "Serbian",
+            "Lithuanian",
+            "Latvian",
+            "Estonian",
         ];
         ui.menu_button("Languages", |ui| {
-            for (lang, code) in languages.iter().zip(language_codes.iter()) {
-                if lang_in == code.to_string() {
-                    if ui
-                        .add(egui::Button::new(
-                            egui::RichText::new(*lang).color(Color32::LIGHT_BLUE),
-                        ))
-                        .clicked()
-                    {
-                        lang_in = code.to_string();
-                        ui.close_menu();
-                    };
-                } else {
-                    if ui.button(*lang).clicked() {
-                        lang_in = code.to_string();
-                        ui.close_menu();
+            egui::ScrollArea::vertical()
+                .max_height(350.0)
+                .show(ui, |ui| {
+                    for (lang, code) in languages.iter().zip(language_codes.iter()) {
+                        if lang_in == code.to_string() {
+                            if ui
+                                .add(egui::Button::new(
+                                    egui::RichText::new(*lang).color(Color32::LIGHT_BLUE),
+                                ))
+                                .clicked()
+                            {
+                                lang_in = code.to_string();
+                                ui.close_menu();
+                            };
+                        } else {
+                            if ui.button(*lang).clicked() {
+                                lang_in = code.to_string();
+                                ui.close_menu();
+                            }
+                        }
                     }
-                }
-            }
+                });
         });
         lang_in
     }
